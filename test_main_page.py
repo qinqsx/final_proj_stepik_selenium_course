803e7d9d49ba583591e85f00160e363f04a02191
AlexeevAM <alexeevam@icloud.com>
2022-02-18 16:56:20 +0300

    added test in test_main_page

--- test_main_page.py
+++ test_main_page.py
@@ -11,6 +11,11 @@ def test_guest_can_go_to_login_page(browser):
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_link()
 
+def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
+    link = "http://selenium1py.pythonanywhere.com"
+    page = MainPage(browser, link)
+    page.open()
+
 
 
 
