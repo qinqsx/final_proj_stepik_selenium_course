8c7d698dd5485a4ea9a342e64d8f494c3108f1da
qinqsx <81359282+qinqsx@users.noreply.github.com>
2022-04-25 11:06:27 +0300

    Update test_product_page.py

--- test_product_page.py
+++ test_product_page.py
@@ -1,9 +1,10 @@
+import pytest
+import time
 from .pages.basket_page import BasketPage
 from .pages.product_page import ProductPage
 from .pages.login_page import LoginPage
 from .pages.locators import ProductPageLocators
-import pytest
-import time
+
 
 """Тесты для product_page с маркерами, о них подробнее в pytest.ini"""
 
